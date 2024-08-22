84fe285bdc3c66294da6f1097a734bca026a24d4
hellolst23 <167504857+hellolst23@users.noreply.github.com>
2024-08-22 18:40:34 +0800

    Update DemandExtraction_test.py

--- layer/DemandExtraction_test.py
+++ layer/DemandExtraction_test.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-    dec: demand extraction 部分
-"""
 import math
 import numpy as np
 import torch as t
@@ -16,12 +13,12 @@ class DemandExtraction(Module):
     def __init__(self, n_demand, n_categories, embedding_dim_c, hidden_size, drop=0.25, predict_catgy_fun = 'mlp',score_act_fun=None, demand_extract='mlp',
                  demand_mask=False, demand_agg="exp"):
         """
-        初始化函数
+
         Args:
-            n_demand: int, 需求数量
-            n_categories: int, 类别节点的个数
-            embedding_dim_c: int, 类别嵌入到维度
-            hidden_size: int, 中间隐层（中间结果层的变量维度）
+            n_demand: int
+            n_categories: int
+            embedding_dim_c: int
+            hidden_size: int
             drop: float, default=0.5
             predict_catgy_fun:str, prediction function: mlp
             score_act_fun: str, None, relu, sigmoid
@@ -35,7 +32,7 @@ class DemandExtraction(Module):
         # if visual_catgy_deamnd:
         #     self.visual_catgy_demand = visual_catgy_deamnd
 
-        self.n_categories = n_categories  # include 补[0]，
+        self.n_categories = n_categories 
         self.n_demand = n_demand
         self.hidden_size = hidden_size
 
@@ -50,7 +47,7 @@ class DemandExtraction(Module):
         self.dropout = nn.Dropout(drop) # NOTE: add drop out
         self.demand_linear = nn.Linear(embedding_dim_c, n_demand * hidden_size,
                                        bias=False)
-        self.key_linear = nn.Linear(embedding_dim_c, hidden_size)  # TODO: 是否要map到M个空间？
+        self.key_linear = nn.Linear(embedding_dim_c, hidden_size)  
 
         self.embedding_position = PositionalEmbedding(n_categories, embedding_dim_c)
 
@@ -71,16 +68,13 @@ class DemandExtraction(Module):
 
 
     def weight_init(self):
-        """
-            初始化该layer自己定义而非子模块的参数
-        """
+
         std = 1.0 / math.sqrt(self.hidden_size)
         self.w_score.data.uniform_(std)
         return None
 
     def compute_demand_score_dot(self, hidden_demand_agg, hidden_key, hidden_key_candidate):
         """
-            demand score 的计算方式，和ppt中的一样
         Args:
             hidden_demand_agg: torch.Tensor, batch_size * n_demand * hidden_size
             hidden_key: torch.Tensor, batch_size * max_session_len * hidden_size
@@ -109,7 +103,6 @@ class DemandExtraction(Module):
 
     def compute_demand_score_mlp(self, hidden_demand_agg, hidden_key, hidden_key_candidate):
         """
-         demand score 的计算方式，使用 mlp
          Args:
              hidden_demand_agg: torch.Tensor, batch_size * n_demand * hidden_size
              hidden_key: torch.Tensor, batch_size * max_session_len  * hidden_size
@@ -133,7 +126,7 @@ class DemandExtraction(Module):
 
         hidden_key_candidate = hidden_key_candidate.view(1, 1, -1, self.hidden_size).repeat(batch_size, self.n_demand,
                                                                                             1,
-                                                                                            1)  # todo 醒来检查这里demand——score 的计算方式
+                                                                                            1)  
         hidden_demand_agg = hidden_demand_agg.view(batch_size, self.n_demand, 1, self.hidden_size).repeat(1, 1, n_items,
                                                                                                           1)
         demand_score_candidate = self.nn_linear_score(torch.cat((hidden_demand_agg, hidden_key_candidate),
@@ -146,7 +139,6 @@ class DemandExtraction(Module):
 
     def forward(self, input, candidate_pool_category, session_last_catgy_index,mask_catgy):
         """
-        模型返回对应的demand score
         Args:
             i
             demand_score：torch.Tensor, dtype=torch.float32  batch_size nput:  sess_categories_batch，torch.Tensor， dtype = torch.int64， batch_size * max_session_len
@@ -174,7 +166,7 @@ class DemandExtraction(Module):
             demand_score_candidate = torch.ones((batch_size, self.n_demand, n_items)).to(device=input.device).div(self.n_demand)
             catgy_score = torch.ones((batch_size, self.n_demand, self.n_categories)).to(device=input.device).div(self.n_demand)
             # demand_score = torch.ones((batch_size, self.n_demand, max_session_len)).to(device=input.device)
-            # demand_score_candidate = torch.ones((batch_size, self.n_demand, n_items)).to(device=input.device) # NOTE: 设置为1
+            # demand_score_candidate = torch.ones((batch_size, self.n_demand, n_items)).to(device=input.device) 
 
             # demand_score: batch_size * n_demand * max_session_len
             # demand_score_candidate: batch_size * n_demand * n_items
@@ -189,8 +181,8 @@ class DemandExtraction(Module):
         if self.demand_agg == "exp":
             hidden_demand_agg = hidden_demand.exp().sum(1).log()  # batch_size * n_demand * hidden_size
         elif self.demand_agg == "mean":
-            hidden_demand_agg = hidden_demand.sum(1)  # 均值聚合session的需求表达
-        elif self.demand_agg == "attention": # attention 聚合
+            hidden_demand_agg = hidden_demand.sum(1)  
+        elif self.demand_agg == "attention": 
             hidden_demand_agg = self.att_demand(hidden_demand, session_last_catgy_index,mask_catgy)
         else:
             print("opt.demand_agg value is wrong !!!!")
@@ -301,20 +293,18 @@ class DemandExtraction(Module):
         loss = torch.mean(torch.stack(sim))
         return loss
 
-# 按照transformer的实现重新更改的demand extraction模块
+
 
 # class DemandExtraction(Module):
 
 #     def __init__(self, n_demand, n_categories, embedding_dim_c, hidden_size, score_act_fun=None, demand_extract='mlp',
 #                  demand_mask=False, demand_agg="exp"):
 #         """
-#         按照transformer的实现重新更改的demand extraction模块
-#         初始化函数
 #         Args:
-#             n_demand: int, 需求数量
-#             n_categories: int, 类别节点的个数
-#             embedding_dim_c: int, 类别嵌入到维度
-#             hidden_size: int, 中间隐层（中间结果层的变量维度）
+#             n_demand: int
+#             n_categories: int
+#             embedding_dim_c: int
+#             hidden_size: int
 #             score_act_fun: str, None, relu, sigmoid
 #             demand_extract: str, mlp, dot
 #             demand_mask: bool, default=False, whether mask demand extraction part
@@ -322,7 +312,7 @@ class DemandExtraction(Module):
 #             demand_agg：str, default="exp"， "mean"
 #         """
 #         super(DemandExtraction, self).__init__()
-#         self.n_categories = n_categories  # include 补[0]，
+#         self.n_categories = n_categories
 #         self.n_demand = n_demand
 #         self.hidden_size = hidden_size
 
@@ -348,16 +338,12 @@ class DemandExtraction(Module):
 #             self.weight_init()  # for self.w_score initialization
 
 #     def weight_init(self):
-#         """
-#         初始化该layer自己定义而非子模块的参数
-#         """
 #         std = 1.0 / math.sqrt(self.hidden_size)
 #         self.w_score.data.uniform_(std)
 #         return None
 
-#     def compute_demand_score_dot(self, hidden_demand_agg, hidden_key, hidden_key_candidate): # todo: 明天写计算demand_score 的方式
+#     def compute_demand_score_dot(self, hidden_demand_agg, hidden_key, hidden_key_candidate):
 #         """
-#         demand score 的计算方式，和ppt中的一样
 #         Args:
 #             hidden_demand_agg: torch.Tensor, batch_size * n_demand * hidden_demand_size
 #             hidden_key: torch.Tensor, batch_size * max_session_len * n_demand * hidden_demand_size
@@ -386,7 +372,6 @@ class DemandExtraction(Module):
 
 #     def compute_demand_score_mlp(self, hidden_demand_agg, hidden_key, hidden_key_candidate):
 #         """
-#          demand score 的计算方式，使用 mlp
 #          Args:
 #              hidden_demand_agg: torch.Tensor, batch_size * n_demand * hidden_size
 #              hidden_key: torch.Tensor, batch_size * max_session_len  * hidden_size
@@ -423,7 +408,6 @@ class DemandExtraction(Module):
 
 #     def forward(self, input, candidate_pool_category):
 #         """
-#         模型返回对应的demand score
 #         Args:
 #             input:  sess_categories_batch，torch.Tensor， dtype = torch.int64， batch_size * max_session_len
 #             candidate_pool_category： torch.Tensor, n_items * 1 , each row such as [category_id], the order is one-to-one mapping candidate_pool_item
@@ -457,7 +441,7 @@ class DemandExtraction(Module):
 #         if self.demand_agg == "exp":
 #             hidden_demand_agg = hidden_demand.exp().sum(1).log()  # batch_size * n_demand * hidden_demand_size
 #         elif self.demand_agg == "mean":
-#             hidden_demand_agg = hidden_demand.sum(1)  # 均值聚合session的需求表达
+#             hidden_demand_agg = hidden_demand.sum(1) 
 #         else:
 #             print("opt.demand_agg value is wrong !!!!")
 #         demand_sim_loss = self.demand_similarity_loss(hidden_demand_agg)
@@ -470,7 +454,7 @@ class DemandExtraction(Module):
 #         # demand_score_candidate: batch_size * n_demand * n_items
 
 #         if self.score_act_fun == 'relu':
-#             demand_score = t.relu(demand_score)  # 为了互信息的BCEWITHLOGITLOSS 的 计算，可以考虑加激活函数demand_score \in [0,1]
+#             demand_score = t.relu(demand_score) 
 #             demand_score_candidate = t.relu(demand_score_candidate)
 #         elif self.score_act_fun == 'sigmoid':
 #             demand_score = t.sigmoid(demand_score)
