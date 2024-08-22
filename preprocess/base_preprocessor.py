493af20bad863fe976401006c2451e5aa85a1f21
hellolst23 <167504857+hellolst23@users.noreply.github.com>
2024-08-22 19:22:58 +0800

    Update base_preprocessor.py

--- preprocess/base_preprocessor.py
+++ preprocess/base_preprocessor.py
@@ -4,8 +4,6 @@
 
 # @File    : base_preprocessor.py
 # Desc:
-其他数据集的处理在这个上面展开
-处理完后的session的存储格式
 1) a session is a list ['user_id', 'start_time', 'end_time', 'item_list', 'category_list', 'target_item', 'target_category']
 2) item_catgy.pkl: save categories corresponding to item id starting from 1.
 """
@@ -25,18 +23,6 @@ from collections import Counter
 import pickle
 # from preprocess import *
 from itertools import chain
-
-"""
-数据处理步骤：
-1. 删除有缺失值的行(一般没有缺失值)
-2. 删除当前items 和 类别 出现次数小于5的 数据（DataFrame 中的rows）
-3. 对 user, item, category id重新编码
-4. 划分数据，删除长度< 2的 session
-5. session的数据增强，一个session划成多个session训练与测试
-6. 划分训练集和测试集，最近的 20% session 作为测试集
-7. 训练集和测试集分别存为.pkl文件
-"""
-
 
 
 def save_data(save_path,session_info,item_category,train_list,test_list):
@@ -76,7 +62,6 @@ def filter_items(df_data, minimum_occurrence):
 
         Returns: df_data: data after filtering
     """
-    # 已改成与物品在不同时间与用户交互的交互数，这样要求一个物品至少与minimum_occurrence个session中
     df_temp = df_data.loc[:, ['user_id','item_id','category_id', 'timestamp']].drop_duplicates()
     item_times = df_temp.loc[:, 'item_id'].value_counts()
     cagry_times = df_temp.loc[:, 'category_id'].value_counts()
@@ -94,7 +79,7 @@ def split_train_test(sess_list, train_size):
     '''
     split train dataset and test dataset by session start time. The latest 20% sessions (before session data enhancement)are the test dataset.
     Args:
-        sess_list: taobao_preprocessor() 的返回值
+
         [(user_id,start_time,end_time,item_list,category_list,target_item,target_category)]
 
     Returns: train_list, test_list
@@ -106,7 +91,7 @@ def split_train_test(sess_list, train_size):
     df = df.sort_values('start_time')
     df_temp = df[['user_id', 'start_time']].sort_index()
     print('before len(df_temp): ', len(df_temp))
-    df_temp = df_temp.drop_duplicates() # 去掉重复值，来算出没经过session 数据增强之前的session总数
+    df_temp = df_temp.drop_duplicates()
     num_total_session = len(df_temp)
     print('after len(df_temp): ', len(df_temp))
     row_index = df_temp.index.tolist()
@@ -147,8 +132,8 @@ def exclude_item_notin_training(train, test, item_category, session_info):
                                'target_category'])
 
     item_train = list(chain(* df_train['item_list'].values.tolist())) + df_train['target_item'].values.tolist()
-    item_train = list(set(item_train)) # 去重复值,
-    item_train.sort(reverse=False)  # 按照item id 升序排列
+    item_train = list(set(item_train)) 
+    item_train.sort(reverse=False)  
     catgy_train = list(chain(* df_train['category_list'].values.tolist())) + df_train['target_category'].values.tolist()
     catgy_train = list(set(catgy_train))
     catgy_train.sort(reverse=False)
@@ -182,11 +167,11 @@ def exclude_item_notin_training(train, test, item_category, session_info):
     df_test_1 = merge2session(df_test)
 
     def filter_item_test(item_list):
-        new_item_list = [item for item in item_list if item in item_train]  # filter_item_test函数外,exclude_item_notin_training函数内的局部变量
+        new_item_list = [item for item in item_list if item in item_train]  
         # if item_list != new_item_list:
         #     print('before filtering : ', item_list)
         #     print('after new item_list: ', new_item_list)
-        if len(new_item_list) < MINIMUM_SESSION_LENGTH: # 全局变量
+        if len(new_item_list) < MINIMUM_SESSION_LENGTH: 
             #print('after filtering remove: ', new_item_list)
             new_item_list = None
         return new_item_list
@@ -204,13 +189,13 @@ def exclude_item_notin_training(train, test, item_category, session_info):
     df_test_1 = df_test_1.dropna()
     print("df_test_1[['session_item', session_catgy']]: \n", df_test_1[['session_item', 'session_catgy']])
 
-    # 重新编码一次
+
     item_encoder = LabelEncoder()
     category_encoder = LabelEncoder()
     item_encoder.fit(item_train)
     category_encoder.fit(catgy_train)
 
-    # 这一段代码与下面注释的两行代码结果一样，因为encoder.fit 是按照item_train中item id 第一次出现的前后顺序来决定编码后的id 的相对大小的
+
     itemId = item_train
     item_transformed = list(item_encoder.transform(itemId))
     item_item = dict(zip(itemId, item_transformed))
@@ -221,7 +206,7 @@ def exclude_item_notin_training(train, test, item_category, session_info):
     item_category_new_1 = dict(sorted(item_category_new.items(), key=lambda item: item[0]))
     print('item_category_new_1 == item_category_new: ', item_category_new_1 == item_category_new)
 
-    item_category_new_1 = (np.array(list(item_category_new_1.values())) + 1).tolist()  # 前面的encoder 默认编码是从0开始的，这里转成从1开始
+    item_category_new_1 = (np.array(list(item_category_new_1.values())) + 1).tolist()  
     print('type(item_category_new_1[0])', type(item_category_new_1[0]))
 
     """
@@ -259,14 +244,14 @@ def get_session(group):
     user_id, df = group
     res = []
     df = df.sort_values('timestamp', ascending=True)  # Sort by time
-    for timestamp, df_t in df.groupby(by='timestamp'):  # 按天划分session  #todo taobao 数据集在这里不同，但是tafeng和tmall一样
+    for timestamp, df_t in df.groupby(by='timestamp'):  
         item_list = df_t['item_id'].tolist()
         category_list = df_t['category_id'].tolist()
         # If the length of a session is less than MINIMUM_SESSION_LENGTH
         if len(item_list) < MINIMUM_SESSION_LENGTH or len(category_list) < MINIMUM_SESSION_LENGTH:
             continue
         if MAXIMUM_LENGTH < len(item_list):  # cut when reach the maximum length
-            #print('Long remove: ', item_list[MAXIMUM_LENGTH + 1: ], 'catgy: ',  category_list[MAXIMUM_LENGTH + 1:]) # todo 这里是否考虑要把截断的部分保留
+            #print('Long remove: ', item_list[MAXIMUM_LENGTH + 1: ], 'catgy: ',  category_list[MAXIMUM_LENGTH + 1:])
             item_list = item_list[:MAXIMUM_LENGTH + 1]
             category_list = category_list[:MAXIMUM_LENGTH + 1]
 
@@ -297,8 +282,8 @@ def base_preprocessor(data, save_path,beizhu='beizhu', sess_enhancement=False,ex
         works: How many workers to used. default to 1
     Returns:
     """
-    print(data.isnull().all())# 判断每一列是否有缺失值
-    # data.dropna(axis=1, how='any', inplace=True) #判断一下数据是否有缺失值，有的删掉
+    print(data.isnull().all())
+    # data.dropna(axis=1, how='any', inplace=True) 
 
     # Filter item and category id
     data = filter_items(data, minimum_occurrence)
@@ -307,15 +292,15 @@ def base_preprocessor(data, save_path,beizhu='beizhu', sess_enhancement=False,ex
     user_encoder = LabelEncoder()
     item_encoder = LabelEncoder()
     category_encoder = LabelEncoder()
-    data['item_id'] = item_encoder.fit_transform(data['item_id']) + 1  # 编码从1开始，适应sr-gnn的源码，这样后面session补成定长的时候可以区分开来
+    data['item_id'] = item_encoder.fit_transform(data['item_id']) + 1  
     data['user_id'] = user_encoder.fit_transform(data['user_id'])
     data['category_id'] = category_encoder.fit_transform(data['category_id']) + 1
-    num_of_user = len(user_encoder.classes_)  # user_encoder.classes_记录未编码前的类别，ndarray
+    num_of_user = len(user_encoder.classes_)  
     num_of_item = len(item_encoder.classes_)
     num_of_category = len(category_encoder.classes_)
     # Get category id for all the items according items id from small to large
     item_category = data.drop_duplicates(subset=['item_id'], keep='first').sort_values('item_id')[
-        'category_id'].tolist()  # 只考虑'item_id'列，将这列对应值相同的行进行去重
+        'category_id'].tolist()  
     session_info = {'beizhu': beizhu,'hyper_minimum_session_length': minimun_session_length,
                     'hyper_maximum_session_length': maximum_length, 'hyper_minimum_occurrence': minimum_occurrence,
                     'hyper_train_size': train_size,
@@ -332,9 +317,9 @@ def base_preprocessor(data, save_path,beizhu='beizhu', sess_enhancement=False,ex
         # Group by user id
     global TIME_INTERVAL, MAXIMUM_LENGTH, MINIMUM_SESSION_LENGTH, SESSION_ENHANCEMENT
     TIME_INTERVAL = time_interval
-    MAXIMUM_LENGTH = maximum_length  # 最长session长度
-    MINIMUM_SESSION_LENGTH = minimun_session_length  # 最短session长度
-    SESSION_ENHANCEMENT = sess_enhancement  # bool,是否进行session data enhancement. Namely, an session is devided into multi sessions
+    MAXIMUM_LENGTH = maximum_length 
+    MINIMUM_SESSION_LENGTH = minimun_session_length  
+    SESSION_ENHANCEMENT = sess_enhancement  # bool
     print('---------------Start splitting sessions--------------------')
     print(' SESSION_ENHANCEMEN: ', SESSION_ENHANCEMENT, '\tMINIMUM_SESSION_LENGTH: ', MINIMUM_SESSION_LENGTH)
     # Multiprocessing
@@ -395,12 +380,12 @@ def load_taobao(file_path, sample_ratio=0.1,nrows=None):
     print("-----------------------Read csv finished.-------------------------@ %ss" % datetime.datetime.now())
     data['timestamp'] = pd.to_datetime(data['timestamp'], unit='s')
     # print(Counter(data['behavior_type']))
-    # data_buy = data[(data['behavior_type'].isin(['buy','cart','fav']))] # NOTE: 新加的 只要buy的数据
+    # data_buy = data[(data['behavior_type'].isin(['buy','cart','fav']))]
     # print(Counter(data_buy['behavior_type']))
     # print(data_buy.head())
     data.drop('behavior_type',axis=1, inplace=True)
 
-    # NOTE: 只有taobao数据集需要采样
+
     user_id = data['user_id'].unique()
     print(f"user_id num: {len(user_id)}")
     sample_num = int(0.1*len(user_id))
@@ -430,13 +415,12 @@ def taobao_main():
     print('time cost', time_end - time_start, 's')
 
 if __name__ == "__main__":
-    # #  预处理代码
 
     # from preprocess.tafeng import load_tafeng
     # os.chdir('../')
     # time_start = time.time()
     # file_load = 'datasets/tafeng/ta_feng.csv'
-    # saving_load = 'datasets/tafeng_test/' # session 的长度最小为3，即除去target之后最小是2 #tafeng_singleThread
+    # saving_load = 'datasets/tafeng_test/' 
     # data = load_tafeng(file_path=file_load, nrows=10000)
     # beizhu = 'only 10000 lines for debug, exclude_item=False, sess_enhancement: False '
     # # beizhu = ' sess_enhancement: True, test set not session enhancement, only for train set; \n' \
