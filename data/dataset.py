a0db9e05ab0c769273b2375acbf8be7d0b2b82b4
hellolst23 <167504857+hellolst23@users.noreply.github.com>
2024-08-22 18:33:11 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -63,7 +63,6 @@ def load_data(dataset_dir, validation_flag=True, valid_portion=0.5):
 
 def process_adj_matrix(solid_adj_matrix):
     '''
-    对原始邻接矩阵进行进一步加工处理
     Args:
         solid_adj_matrix: max_nodes_len*max_nodes_len
     Returns:
@@ -75,11 +74,6 @@ def process_adj_matrix(solid_adj_matrix):
 
 def sess_collate_fn(batch):
     '''
-    1）一个batch 数据, 定长处理
-        i）sess_categories 处理成定长 max_session_len （一个batch中，session长度的最大值）
-        ii）sess_items 后相异节点的个数 处理成定长 max_nodes_len（一个batch中，session不同节点数的最大值）
-    2）获取sess_items 对应的一阶邻接矩阵 adj_matrix 维度：max_nodes_len*max_nodes_len
-    3) 获取 session中相异节点与其sess_categories 的一一对应矩阵（映射关系） nodes_categories_matrix 维度：max_nodes_len*max_session_len
     Args:
         batch: a list, [((sess_items, sess_categories), (target_item, target_item_category)), ...  ], len = batch_size
     Returns:
@@ -96,12 +90,12 @@ def sess_collate_fn(batch):
     sess_nodes_batch = []
     adj_matrix_batch = []
     nodes_categories_matrix_batch = []
-    mask_item_batch = [] # 存 unique nodes 序列的 mask矩阵
-    mask_catgy_batch = []  # 存 catgy 序列的 mask矩阵
+    mask_item_batch = [] 
+    mask_catgy_batch = []  
     session_last_item_index_batch = []
     session_last_catgy_index_batch = []
-    num_nodes = []  # 存一个session相异节点数
-    num_sess_batch = []  # 存一个session的长度
+    num_nodes = []  
+    num_sess_batch = []  
 
     for sess in batch:
         sess_items = sess[0][0]
@@ -112,7 +106,7 @@ def sess_collate_fn(batch):
 
     for session, target in batch:
         sess_target_batch.append(target)
-        # 补成定长
+
         sess_catgories = session[1]
         sess_categories_batch.append(sess_catgories + ((max_session_len - len(sess_catgories)) * [0]))
         session_last_catgy_index_batch.append(len(sess_catgories) - 1)
@@ -123,10 +117,6 @@ def sess_collate_fn(batch):
         sess_nodes_batch.append(unique_items.tolist() + ((max_nodes_len - len(unique_items)) * [0]))
         session_last_item_index_batch.append((np.where(unique_items == last_item))[0][0])  # Get the index in unique_items
 
-        # todo: 加last node index
-        # 获取邻接矩阵, not self-loop, namely, a_ii != 1, unless (v_i, v_i)appears
-        # 获取相异节点与其sess_categories 的映射矩阵 nodes_categories_matrix
-        # 获取mask矩阵
         solid_adj_matrix = np.zeros((max_nodes_len, max_nodes_len))
         nodes_categories_matrix = np.zeros((max_nodes_len, max_session_len))
         mask = np.zeros(max_nodes_len)
@@ -136,14 +126,14 @@ def sess_collate_fn(batch):
         for i in np.arange(sess_len - 1):
             mask_catgy[i] = 1
             u = np.where(unique_items == sess_items[i])[0][
-                0]  # np.where() 返回一个元组， 元组元素只有一个，为一个array数组， 记录node array数组中值为session item id 的索引
+                0] 
             if not mask[u]: 
                 mask[u] = 1
             v = np.where(unique_items == sess_items[i + 1])[0][0]
             if not solid_adj_matrix[u][v]:
-                solid_adj_matrix[u][v] = 1  # TODO: 是否要考虑多种边的情况
+                solid_adj_matrix[u][v] = 1  
             if not nodes_categories_matrix[u][i]:
-                nodes_categories_matrix[u][i] = 1 # 每一行 sum 不一定等于1， 等于该node在session中出现的次数
+                nodes_categories_matrix[u][i] = 1 
 
         solid_adj_matrix = process_adj_matrix(solid_adj_matrix)
         adj_matrix_batch.append(solid_adj_matrix)
@@ -184,13 +174,12 @@ class RsData(data.Dataset):
 
     def get_adj_matrix(self, sess_items, sliding_size=2):
         '''
-        获取一个session的邻接矩阵
         Args:
             sess_items: [item_id_0, ... ], 一个session序列
 
         Returns:
             adj_matrix：(solid_adj_matrix, dashed_adj_matrix), dtype = np.array
-            the adjacency matrix of a session, 索引按照该session的item_id 从小到大的顺序排列, 实线连接(有向，只算了入度的)，和虚线连接的分别用一个矩阵存起来
+            the adjacency matrix of a session
             example： sess_items = [6,3,4,3,1,5]
             solid_adj_matrix = [[0,0,0,0,1],
                                 [1,0,1,0,0]，
@@ -207,12 +196,12 @@ class RsData(data.Dataset):
         m_len = len(unique_items)
         solid_adj_matrix = np.zeros((m_len, m_len))
         dashed_adj_matrix = np.zeros((m_len, m_len))
-        for i in np.arange(len(sess_items) - 1):  # TODO 原始邻接矩阵，没有做其他处理
+        for i in np.arange(len(sess_items) - 1): 
             u = np.where(unique_items == sess_items[i])[0][
-                0]  # np.where() 返回一个元组， 元组元素只有一个，为一个array数组， 记录node array数组中值为session item id 的索引
+                0] 
             v = np.where(unique_items == sess_items[i + 1])[0][0]
             solid_adj_matrix[u][v] += 1
-            if sliding_size < 2:  # TODO 原始虚线邻接矩阵，按滑动窗口，没有做其他处理
+            if sliding_size < 2:  
                 raise Exception('sliding_size < 2, there are not dashed edges ')
             else:
                 if i <= len(sess_items) - 1 - sliding_size:
